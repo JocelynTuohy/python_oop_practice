1f68fdfaea843652f48febbc0ec99af04a6213e5
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-06-07 18:13:03 -0700

    began Hospital assignment (Coding Dojo)

---
+++ hospital.py
@@ -0,0 +1,11 @@
+# Hospital Assignment
+# You're going to build a hospital with patients in it! Create a hospital class.
+class Hospital(object):
+    def __init__(self):
+        self.patients = []
+        self.name = "St. Mary's Hospital"
+        self.capacity = 15
+
+class Patient(object):
+    def __init__(self):
+        
